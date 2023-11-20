fe915cbd9243c0e119159648338d653fd40fb983
ArtemChuban <ArtemChuban@yandex.ru>
2023-11-20 13:52:36 +0200

    ITN and MSRN

--- backend/app/models/PointBase.py
+++ backend/app/models/PointBase.py
@@ -4,3 +4,5 @@ from pydantic import BaseModel, Field
 class PointBase(BaseModel):
     title: str = Field(min_length=1)
     address: str = Field(min_length=1)
+    ITN: str = Field(min_length=10, max_length=12)
+    MSRN: str = Field(min_length=13, max_length=13)
--- backend/app/models/PointUpdate.py
+++ backend/app/models/PointUpdate.py
@@ -5,3 +5,5 @@ from pydantic import BaseModel, Field
 class PointUpdate(BaseModel):
     title: Optional[str] = Field(min_length=1, default=None)
     address: Optional[str] = Field(min_length=1, default=None)
+    ITN: Optional[str] = Field(min_length=10, max_length=12, default=None)
+    MSRN: Optional[str] = Field(min_length=13, max_length=13, default=None)
