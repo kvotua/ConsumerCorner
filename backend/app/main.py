6c429128d40eacbe66d8496cd78e0a38591a5057
ArtemChuban <ArtemChuban@yandex.ru>
2023-11-20 13:03:27 +0200

    Refactoring

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ import qrcode.image.svg
 import uuid
 from typing import Annotated
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response, RedirectResponse
 from fastapi_limiter import FastAPILimiter
@@ -54,52 +54,81 @@ CODE_EXPIRES_SECONDS = 60 * 15
 POINT_PRICE = 100 * 100
 
 
-@app.post("/comments", tags=["Comments"])
-async def post_comment(comment: CommentBase) -> None:
-    point = points_collection.find_one({"id": comment.pointID})
+def proprietorID_by_token(token: Token) -> ProprietorID:
+    proprietorID = await redis_database.get(f"token:{token}")
+    if proprietorID is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Wrong token")
+    return proprietorID
+
+
+def point_exist(pointID: PointID) -> bool:
+    return points_collection.find_one({"id": pointID}) is not None
+
+
+def point_by_id(pointID: PointID) -> Point:
+    point = points_collection.find_one({"id": pointID})
     if point is None:
         raise HTTPException(status_code=404, detail="Wrong pointID")
+    point = Point(**point)
+
+
+def comments_by_pointID(pointID: PointID) -> list[Comment]:
+    comments = comments_collection.find({"pointID": pointID})
+    return [Comment(**x) for x in comments]
+
+
+def proprietor_by_id(proprietorID: ProprietorID) -> Proprietor:
+    proprietor_model = proprietors_collection.find_one({"id": proprietorID})
+    if proprietor_model is None:
+        raise HTTPException(status_code=500, detail="Proprietor not exist")
+    return Proprietor(**proprietor_model)
+
+
+def proprietor_by_token(token: Token) -> Proprietor:
+    proprietorID = proprietorID_by_token(token)
+    return proprietor_by_id(proprietorID)
+
+
+@app.post("/comments", tags=["Comments"])
+async def post_comment(comment: CommentBase) -> None:
+    if not point_exist(comment.pointID):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Wrong pointID"
+        )
     comment = Comment(**comment.model_dump())
     comments_collection.insert_one(comment.model_dump())
 
 
 @app.get("/comments/by/pointID", tags=["Comments"])
 async def get_comments(token: Token, pointID: PointID) -> list[Comment]:
-    proprietorID = await redis_database.get(f"token:{token}")
-    if proprietorID is None:
-        raise HTTPException(status_code=404, detail="Wrong token")
-    point = points_collection.find_one({"id": pointID})
-    if point is None:
-        raise HTTPException(status_code=404, detail="Wrong pointID")
-    point = Point(**point)
+    proprietorID = proprietorID_by_token(token)
+    point = point_by_id(pointID)
     if point.owner != proprietorID:
-        raise HTTPException(status_code=403)
-    comments = comments_collection.find({"pointID": point.id})
-    comments = [Comment(**x) for x in comments]
-    return comments
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Only owner can read comments"
+        )
+    return comments_by_pointID(pointID)
 
 
 @app.post("/points", tags=["Points"])
 async def post_point(token: Token, point: PointBase) -> None:
-    proprietorID = await redis_database.get(f"token:{token}")
-    if proprietorID is None:
-        raise HTTPException(status_code=404, detail="Wrong token")
-    proprietor_model = proprietors_collection.find_one({"id": proprietorID})
-    if proprietor_model is None:
-        raise HTTPException(status_code=500, detail="Proprietor not exist")
-    proprietor = Proprietor(**proprietor_model)
+    proprietor = proprietor_by_token(token)
     if proprietor.balance < POINT_PRICE:
         raise HTTPException(status_code=400, detail="Insufficient funds")
-    point = Point(**point.model_dump(), owner=proprietorID)
+    point = Point(**point.model_dump(), owner=proprietor.id)
     points_collection.insert_one(point.model_dump())
     proprietors_collection.update_one(
-        {"id": proprietorID},
+        {"id": proprietor.id},
         {"$push": {"points_id": point.id}, "$inc": {"balance": -POINT_PRICE}},
     )
 
 
 @app.get("/points/{pointID}/qr", tags=["Points"])
 async def get_point_qr(pointID: PointID) -> Response:
+    if not point_exist(pointID):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Wrong pointID"
+        )
     data = qrcode.make(
         f"http://xn--90abdibneekjf0abcbbqil3bejr0c1r.xn--p1ai/points/{pointID}",
         image_factory=qrcode.image.svg.SvgImage,
@@ -109,12 +138,15 @@ async def get_point_qr(pointID: PointID) -> Response:
 
 @app.delete("/points", tags=["Points"])
 async def delete_point(token: Token, pointID: PointID) -> None:
-    proprietorID = await redis_database.get(f"token:{token}")
-    if proprietorID is None:
-        raise HTTPException(status_code=404, detail="Wrong token")
-    p = points_collection.find_one_and_delete({"id": pointID})
-    if p is None:
-        raise HTTPException(status_code=404, detail="Wrong pointID")
+    if not point_exist(pointID):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Wrong pointID"
+        )
+    proprietorID = proprietorID_by_token(token)
+    if points_collection.find_one({"id": pointID, "owner": proprietorID}) is None:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Only owner can delete point"
+        )
     comments_collection.delete_many({"pointID": pointID})
     proprietors_collection.update_one(
         {"id": proprietorID}, {"$pull": {"points_id": pointID}}
@@ -123,60 +155,37 @@ async def delete_point(token: Token, pointID: PointID) -> None:
 
 @app.patch("/points", tags=["Points"])
 async def patch_point(token: Token, pointID: PointID, updates: PointUpdate) -> None:
-    proprietorID = await redis_database.get(f"token:{token}")
-    if proprietorID is None:
-        raise HTTPException(status_code=404, detail="Wrong token")
+    proprietorID = proprietorID_by_token(token)
     result = points_collection.update_one(
-        {"id": pointID}, {"$set": updates.model_dump(exclude_none=True)}
+        {"id": pointID, "owner": proprietorID},
+        {"$set": updates.model_dump(exclude_none=True)},
     )
     if result.matched_count < 1:
-        raise HTTPException(status_code=404, detail="Wrong pointID")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Wrong pointID"
+        )
 
 
 @app.get("/points", tags=["Points"])
 async def get_point(pointID: PointID) -> Point:
-    point = points_collection.find_one({"id": pointID})
-    if point is None:
-        raise HTTPException(status_code=404, detail="Wrong pointID")
-    point = Point(**point)
-    return point
+    return point_by_id(pointID)
 
 
 @app.patch("/proprietors", tags=["Proprietors"])
 async def patch_proprietor(token: Token, updates: ProprietorUpdate) -> None:
-    proprietorID = await redis_database.get(f"token:{token}")
-    if proprietorID is None:
-        raise HTTPException(status_code=404, detail="Wrong token")
-    if updates.phone_number is not None and updates.code is None:
-        raise HTTPException(
-            status_code=400, detail="Code required for change phone number"
-        )
+    proprietorID = proprietorID_by_token(token)
     updates_dict = updates.model_dump(exclude_none=True)
-    if updates.phone_number is not None:
-        code = codes_collection.find_one_and_delete(
-            {"phone_number": updates.phone_number, "value": updates_dict.pop("code")}
-        )
-        if code is None:
-            raise HTTPException(status_code=400, detail="Wrong code")
     proprietors_collection.update_one({"id": proprietorID}, {"$set": updates_dict})
 
 
 @app.get("/proprietors/by/id", tags=["Proprietors"])
 async def get_proprietor_by_id(proprietorID: ProprietorID) -> Proprietor:
-    proprietor = proprietors_collection.find_one({"id": proprietorID})
-    if proprietor is None:
-        raise HTTPException(status_code=404, detail="Wrong proprietorID")
-    proprietor = Proprietor(**proprietor)
-    return proprietor
+    return proprietor_by_id(proprietorID)
 
 
 @app.get("/proprietors/by/token", tags=["Proprietors"])
-async def get_proprietor(token: Token) -> Proprietor:
-    proprietorID = await redis_database.get(f"token:{token}")
-    if proprietorID is None:
-        raise HTTPException(status_code=404, detail="Wrong token")
-    proprietor = Proprietor(**proprietors_collection.find_one({"id": proprietorID}))
-    return proprietor
+async def get_proprietor_by_token(token: Token) -> Proprietor:
+    return proprietor_by_token(token)
 
 
 @app.post("/proprietors", tags=["Proprietors"])
