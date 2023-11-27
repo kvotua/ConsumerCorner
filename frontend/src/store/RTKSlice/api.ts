89f20f7fb9648d3a921f86bec5935cb3d64fd0a5
eugene <hom.1232015@yandex.ru>
2023-11-27 11:07:30 +0200

    fix req

--- frontend/src/store/RTKSlice/api.ts
+++ frontend/src/store/RTKSlice/api.ts
@@ -12,7 +12,7 @@ export const ugolokApi = createApi({
     reducerPath: "ugolokApi",
     tagTypes: ["getUser", "addPoint", "getPoints"],
     baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_API_URL,
+        baseUrl: 'http://xn--90abdibneekjf0abcbbqil3bejr0c1r.xn--p1ai:8000',
     }),
     endpoints: (builder) => ({
         getUser: builder.query({
