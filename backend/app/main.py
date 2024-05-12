3005d66ad8552334466a68c6e1c3da586e7a8fe7
Artem Chuban <ArtemChuban@yandex.ru>
2024-05-12 16:10:52 +0200

    misc

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,6 @@
+import os
+
 from fastapi import FastAPI
 
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+debug = os.getenv("DEBUG") is not None
+app = FastAPI(debug=debug)
