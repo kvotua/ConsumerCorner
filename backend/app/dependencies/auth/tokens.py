a055cc760b0cb87b0fe15614437f94e6ba5ca3fa
Popov Nikita <popovnikita01@gmail.com>
2024-09-16 19:35:19 +0200

    получение таблицы access_tokens

---
+++ backend/app/dependencies/auth/tokens.py
@@ -0,0 +1,16 @@
+from typing import TYPE_CHECKING, Annotated
+
+from core.models import AccessToken, db_helper
+from fastapi import Depends
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def get_access_tokens_db(
+    session: Annotated[
+        "AsyncSession",
+        Depends(db_helper.session_getter),
+    ],
+):
+    yield AccessToken.get_db(session=session)
