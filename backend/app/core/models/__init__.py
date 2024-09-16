8d1301d494f727b4498cfe882e63b1cd7fb092e6
Popov Nikita <popovnikita01@gmail.com>
2024-09-16 20:09:42 +0200

    описал модель Enterprise

--- backend/app/core/models/__init__.py
+++ backend/app/core/models/__init__.py
@@ -1,6 +1,13 @@
-__all__ = ("db_helper", "Base", "User", "AccessToken")
+__all__ = (
+    "db_helper",
+    "Base",
+    "User",
+    "AccessToken",
+    "Enterprise",
+)
 
 from .base import Base
 from .db_helper import db_helper
+from .enterprise import Enterprise
 from .token import AccessToken
 from .user import User
