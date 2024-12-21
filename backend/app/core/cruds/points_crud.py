c55200ac54fcdf33ce3d4ad1b964089f0c4d69f5
pla1keo <plalkeo@gmail.com>
2024-12-21 02:00:14 +0200

    fix for getting socials in point by id

--- backend/app/core/cruds/points_crud.py
+++ backend/app/core/cruds/points_crud.py
@@ -137,7 +137,7 @@ async def get_point_by_id(session: AsyncSession, point_id: int) -> PointInfo:
     result_2 = await session.execute(stmt_2)
     social_data = result_2.scalars().all()
     social_data_dicts = [
-        {"social_id": social.id, "name": social.name, "link": social.link} for social in social_data
+        {"social_id": social.id, "name": social.name, "link": social.link} for social_point, social in social_data
     ]
 
     stmt_3 = select(Docs).where(Docs.point_id == point_id)
