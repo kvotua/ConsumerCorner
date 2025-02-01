ef4002044e7f700fd3bda818e56f4877de83a9ad
pla1keo <plalkeo@gmail.com>
2025-02-01 15:31:53 +0200

    added display name, count comments with bell and fully or not Socials and Documents in MenuPage

--- FrontendExpo/app/(tabs)/ProfilePages/MenuPage.tsx
+++ FrontendExpo/app/(tabs)/ProfilePages/MenuPage.tsx
@@ -21,18 +21,18 @@ const isTablet = width >= 768;
 
 export default function Menupage({ navigation }) {
   const [toast, setToast] = useState({ type: "", message: "", subMessage: "", visible: false });
+  const [info, setInfo] = useState({});
   const showToast = (type: string, message: string, subMessage: string) => {
     setToast({ type, message, subMessage, visible: true });
     setTimeout(() => setToast({ ...toast, visible: false }), 3000);
   };
-  var info = {}
-  // useEffect(() => {
-  //   fetchReviews();
-  // }, []);
-  const fetchReviews = async () => {
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+  const fetchInfo = async () => {
     try {
-      const token = AccessGetToken();
-      const url = `https://consumer-corner.kvotua.ru/enterprises/get-users`;
+      const token = await AccessGetToken();
+      const url = `https://consumer-corner.kvotua.ru/profile/docs_socials_exist`;
       const data = await fetch(url, {
         method: 'GET',
         headers: {
@@ -40,8 +40,14 @@ export default function Menupage({ navigation }) {
           'Authorization': `Bearer ${token}`,
         }
       })
-      const res = data.json();
-      info = res;
+      const res = await data.json();
+      console.log(res)
+      if (res.name) {
+        setInfo(res);
+      } else {
+        const text = res.detail || "Неизвестная ошибка";
+        showToast("error", "Ошибка!", text);
+      }
     } catch (error) {
       showToast("error", "Ошибка!", error.message || "Произошла неизвестная ошибка.");
     }
@@ -51,33 +57,53 @@ export default function Menupage({ navigation }) {
   return (
     <ImageBackground source={require("../../../assets/images/background.png")} style={styles.background}>
       <SafeAreaView style={styles.containerMainPage}>
-      <View style={[styles.menuHeader, { marginTop: Platform.OS === 'ios' ? 10 : 50, height: 80,justifyContent: 'flex-end', }]}>
-      <Text style={styles.menuTitle} >Здравствуйте, Екатерина</Text>
+        <View style={[styles.menuHeader, { marginTop: Platform.OS === 'ios' ? 10 : 50, height: 80, justifyContent: 'flex-end', }]}>
+          <Text style={styles.menuTitle} >Здравствуйте, {info.name}</Text>
         </View>
 
         <View style={styles.menuTopButtons}>
           <TouchableOpacity
-            style={styles.whiteButtonMenuTopNoActive}
+            style={info.documents ? styles.whiteButtonMenuTopActive : styles.whiteButtonMenuTopNoActive}
             onPress={() => navigation.replace("Documents")}
           >
-          <View style={[{flex: 1, justifyContent: "space-between",}]}>
-          <Text style={[styles.blackText, {paddingTop: isTablet ? 30 : 25, paddingStart: 15}]}>Документы</Text>
-            <Text style={[styles.alertText, { paddingStart: 15, }]}>Не заполнены</Text>
-            <Image
-            source={require("../../../assets/images/corner.png")}
-            style={[{ width: 25, height: 25, alignSelf: "flex-end", bottom: 20, right: 10}]}
-          />
-        </View>
+            <View style={{ flex: 1, justifyContent: "space-between" }}>
+              <Text style={[styles.blackText, { paddingTop: isTablet ? 30 : 25, paddingStart: 15 }]}>Документы</Text>
+              {!info.documents && (
+                <Text style={[styles.alertText, { paddingStart: 15 }]}>Не заполнены</Text>
+              )}
+              <Image
+                source={require("../../../assets/images/corner.png")}
+                style={{ width: 25, height: 25, alignSelf: "flex-end", bottom: info.documents ? 10 : 20, right: 10 }}
+              />
+            </View>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.whiteButtonMenuTopActive} onPress={() => navigation.replace("Social")}>
-          <View style={[{flex: 1, justifyContent: "space-between",}]}>
-            <Text style={styles.blackText}>Соц. сети</Text>
-            <Image
-            source={require("../../../assets/images/corner.png")}
-            style={[{ width: 25, height: 25, alignSelf: "flex-end", bottom: 10, right: 10}]}
-          />
-          </View>
+          <TouchableOpacity
+            style={info.socials ? styles.whiteButtonMenuTopActive : styles.whiteButtonMenuTopNoActive}
+            onPress={() => navigation.replace("Social")}
+          >
+            <View style={{ flex: 1, justifyContent: "space-between" }}>
+              <Text style={[styles.blackText, { paddingTop: isTablet ? 30 : 25, paddingStart: 15 }]}>Соц. сети</Text>
+              {!info.socials && (
+                <Text style={[styles.alertText, { paddingStart: 15 }]}>Не заполнены</Text>
+              )}
+              <Image
+                source={require("../../../assets/images/corner.png")}
+                style={{ width: 25, height: 25, alignSelf: "flex-end", bottom: info.socials ? 10 : 20, right: 10 }}
+              />
+            </View>
           </TouchableOpacity>
+          {/* <TouchableOpacity
+            style={styles.whiteButtonMenuTopActive}
+            onPress={() => navigation.replace("Social")}
+          >
+          <View style={[{ flex: 1, justifyContent: "space-between", }]}>
+            <Text style={[styles.blackText, { paddingTop: isTablet ? 30 : 25, paddingStart: 15 }]}>Соц. сети</Text>
+              <Image
+                source={require("../../../assets/images/corner.png")}
+                style={[{ width: 25, height: 25, alignSelf: "flex-end", bottom: 20, right: 10 }]}
+              />
+            </View>
+          </TouchableOpacity> */}
         </View>
 
         <View style={styles.menuBottomButtons}>
@@ -85,24 +111,30 @@ export default function Menupage({ navigation }) {
             style={styles.whiteButtonMenuBottomActive}
             onPress={() => navigation.replace("Reviews")}
           >
-            <View style={[{flex: 1, justifyContent: "space-between",}]}>
-                <Text style={styles.blackText}>Отзывы и предложения</Text>
-                <View style={styles.containerBell}>  
-                  {/* <Text style={[{fontSize: 10, fontWeight: "bold", fontFamily: "Montserrat", textAlign: "right", top: 4}]}>4</Text> */}
-                  {/* <Icons style={[{alignSelf: "flex-end"}]} name="bell" size={22} color="black" /> */}
-            </View>
-            <Image
+            <View style={[{ flex: 1, justifyContent: "space-between", }]}>
+              <Text style={styles.blackText}>Отзывы и предложения</Text>
+              <View style={styles.containerBell}>
+                {info.comments > 0 && (
+                  <>
+                    <Text style={[{ fontSize: 10, fontWeight: "bold", fontFamily: "Montserrat", textAlign: "right", top: 4 }]}>
+                      {info.comments}
+                    </Text>
+                    <Icons style={[{ alignSelf: "flex-end" }]} name="bell" size={22} color="black" />
+                  </>
+                )}
+              </View>
+              <Image
                 source={require("../../../assets/images/corner.png")}
-                style={[{ width: 25, height: 25, alignSelf: "flex-end", bottom: 10, right: 10}]} 
+                style={[{ width: 25, height: 25, alignSelf: "flex-end", bottom: 10, right: 10 }]}
               />
             </View>
           </TouchableOpacity>
           <TouchableOpacity style={styles.whiteButtonMenuBottomActive} onPress={() => navigation.replace("Firms")}>
-            <View style={[{flex: 1, justifyContent: "space-between",}]}>
-                <Text style={styles.blackText}>Фирмы/точки</Text>
-                <Image
+            <View style={[{ flex: 1, justifyContent: "space-between", }]}>
+              <Text style={styles.blackText}>Фирмы/точки</Text>
+              <Image
                 source={require("../../../assets/images/corner.png")}
-                style={[{ width: 25, height: 25, alignSelf: "flex-end", bottom: 10, right: 10}]} 
+                style={[{ width: 25, height: 25, alignSelf: "flex-end", bottom: 10, right: 10 }]}
               />
             </View>
           </TouchableOpacity>
@@ -113,24 +145,24 @@ export default function Menupage({ navigation }) {
             style={styles.whiteButtonMenuBottomActive}
             onPress={() => navigation.replace("AdminPanel")}
           >
-          <View style={[{flex: 1, justifyContent: "space-between",}]}>
-            <Text style={[styles.blackText, { paddingEnd: 24 }]}>Команда</Text>
-            <Image
-            source={require("../../../assets/images/corner.png")}
-            style={[{ width: 25, height: 25, alignSelf: "flex-end", bottom: 10, right: 10}]}/>
-          </View>
+            <View style={[{ flex: 1, justifyContent: "space-between", }]}>
+              <Text style={[styles.blackText, { paddingEnd: 24 }]}>Команда</Text>
+              <Image
+                source={require("../../../assets/images/corner.png")}
+                style={[{ width: 25, height: 25, alignSelf: "flex-end", bottom: 10, right: 10 }]} />
+            </View>
           </TouchableOpacity>
-          <TouchableOpacity 
-            style={styles.whiteButtonMenuBottomActive} 
+          <TouchableOpacity
+            style={styles.whiteButtonMenuBottomActive}
             onPress={() => navigation.replace("Profile")}
           >
-            <View style={[{flex: 1, justifyContent: "space-between",}]}>
-            <Text style={[styles.blackText, {paddingEnd: 24}]}>Профиль</Text>
-            <Image
-            source={require("../../../assets/images/corner.png")}
-            style={[{ width: 25, height: 25, alignSelf: "flex-end", bottom: 10, right: 10}]} 
-          />
-          </View>
+            <View style={[{ flex: 1, justifyContent: "space-between", }]}>
+              <Text style={[styles.blackText, { paddingEnd: 24 }]}>Профиль</Text>
+              <Image
+                source={require("../../../assets/images/corner.png")}
+                style={[{ width: 25, height: 25, alignSelf: "flex-end", bottom: 10, right: 10 }]}
+              />
+            </View>
           </TouchableOpacity>
         </View>
 
@@ -147,7 +179,7 @@ export default function Menupage({ navigation }) {
 
 const localStyles = StyleSheet.create({
   footerText: {
-    fontSize: isTablet ? 16 : 12 ,
+    fontSize: isTablet ? 16 : 12,
     color: "#EEF3FF",
     textAlign: "center",
     fontFamily: 'Montserrat',
@@ -159,11 +191,11 @@ const localStyles = StyleSheet.create({
     justifyContent: 'flex-end',
   },
   menuAdminButtons: {
-      flexDirection: "row",
-      flexWrap: "wrap",
-      width: "100%",
-      gap: 10,
-      marginTop: 10,
-      justifyContent: 'space-between', 
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: "100%",
+    gap: 10,
+    marginTop: 10,
+    justifyContent: 'space-between',
   },
 });
